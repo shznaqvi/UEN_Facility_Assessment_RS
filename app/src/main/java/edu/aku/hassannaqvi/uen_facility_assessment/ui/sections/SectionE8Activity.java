43b1c490a53ebf90c2e4a86b6e18d643fa86f70f
SAJID LATIF <indus1530@gmail.com>
2022-02-10 04:32:41 +1400

    SectionE8Activity functionality

--- app/src/main/java/edu/aku/hassannaqvi/uen_facility_assessment/ui/sections/SectionE8Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_facility_assessment/ui/sections/SectionE8Activity.java
@@ -2,14 +2,28 @@ package edu.aku.hassannaqvi.uen_facility_assessment.ui.sections;
 
 import static edu.aku.hassannaqvi.uen_facility_assessment.core.MainApp.form;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.os.Bundle;
+import com.validatorcrawler.aliazaz.Validator;
 
+import org.json.JSONException;
+
+import edu.aku.hassannaqvi.uen_facility_assessment.MainActivity;
 import edu.aku.hassannaqvi.uen_facility_assessment.R;
+import edu.aku.hassannaqvi.uen_facility_assessment.contracts.TableContracts;
+import edu.aku.hassannaqvi.uen_facility_assessment.core.MainApp;
 import edu.aku.hassannaqvi.uen_facility_assessment.database.DatabaseHelper;
 import edu.aku.hassannaqvi.uen_facility_assessment.databinding.ActivitySectionE8Binding;
+import edu.aku.hassannaqvi.uen_facility_assessment.ui.SectionMainActivity;
+
 
 public class SectionE8Activity extends AppCompatActivity {
 
@@ -17,12 +31,54 @@ public class SectionE8Activity extends AppCompatActivity {
     ActivitySectionE8Binding bi;
     private DatabaseHelper db;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section_e8);
-        //bi.setCallback(this);
+        db = MainApp.appInfo.dbHelper;
+        setSupportActionBar(bi.toolbar);
+        if (MainApp.superuser) bi.btnContinue.setText("Review Next");
         bi.setForm(form);
+    }
+
+
+    private boolean updateDB() {
+        if (MainApp.superuser) return true;
+
+        db = MainApp.appInfo.getDbHelper();
+        long updcount = 0;
+        try {
+            updcount = db.updatesFormColumn(TableContracts.FormsTable.COLUMN_SE, form.sEtoString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d(TAG, R.string.upd_db + e.getMessage());
+            Toast.makeText(this, R.string.upd_db + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if (updcount > 0) return true;
+        else {
+            Toast.makeText(this, R.string.upd_db_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    public void btnContinue(View view) {
+        bi.llbtn.setVisibility(View.GONE);
+        new Handler().postDelayed(() -> bi.llbtn.setVisibility(View.VISIBLE), 5000);
+        if (!formValidation()) return;
+        if (updateDB()) {
+            finish();
+            startActivity(new Intent(this, SectionMainActivity.class));
+        } else Toast.makeText(this, R.string.fail_db_upd, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void btnEnd(View view) {
+        finish();
+        startActivity(new Intent(this, MainActivity.class).putExtra("complete", false));
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
     }
 }
