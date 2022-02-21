b4b9d17e9241ad594f60dea120d740702456202c
SAJID LATIF <indus1530@gmail.com>
2022-02-21 12:40:24 +0500

    ModuleI identificationVariables

--- app/src/main/java/edu/aku/hassannaqvi/uen_facility_assessment/models/ModuleI.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_facility_assessment/models/ModuleI.java
@@ -32,9 +32,9 @@ public class ModuleI extends BaseObservable implements Observable {
     // APP VARIABLES
     private String id = _EMPTY_;
     private String uid = _EMPTY_;
+    private String uuid = _EMPTY_;
     private String userName = _EMPTY_;
     private String sysDate = _EMPTY_;
-    private String cluster = _EMPTY_;
     private String deviceId = _EMPTY_;
     private String deviceTag = _EMPTY_;
     private String appver = _EMPTY_;
@@ -43,6 +43,10 @@ public class ModuleI extends BaseObservable implements Observable {
     private String iStatus96x = _EMPTY_;
     private String synced = _EMPTY_;
     private String syncDate = _EMPTY_;
+    private String districtCode = _EMPTY_;
+    private String tehsilCode = _EMPTY_;
+    private String ucCode = _EMPTY_;
+    private String hfCode = _EMPTY_;
 
 
     // FIELD VARIABLES
@@ -118,13 +122,10 @@ public class ModuleI extends BaseObservable implements Observable {
         setAppver(MainApp.versionName + "." + MainApp.versionCode);
         setUserName(MainApp.user.getUserName());
         setSysDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH).format(new Date().getTime()));
-        /*setProvinceCode(selectedProvince);
-        setDistrictCode(selectedDistrict);
-        setTehsilCode(selectedTehsil);
-        setUcCode(selectedUc);
-        setVillageCode(selectedVillage);
-        setPsuCode(selectedPSU);
-        setHhid(selectedHHID);*/
+        setDistrictCode(MainApp.moduleA.getDistrictCode());
+        setTehsilCode(MainApp.moduleA.getTehsilCode());
+        setUcCode(MainApp.moduleA.getUcCode());
+        setHfCode(MainApp.moduleA.getHfCode());
     }
 
 
@@ -152,14 +153,13 @@ public class ModuleI extends BaseObservable implements Observable {
         this.uid = uid;
     }
 
-    @Bindable
-    public String getCluster() {
-        return cluster;
+
+    public String getUuid() {
+        return uuid;
     }
 
-    public void setCluster(String cluster) {
-        this.cluster = cluster;
-        notifyPropertyChanged(BR.cluster);
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
 
 
@@ -241,6 +241,42 @@ public class ModuleI extends BaseObservable implements Observable {
 
     public void setSyncDate(String syncDate) {
         this.syncDate = syncDate;
+    }
+
+
+    public String getDistrictCode() {
+        return districtCode;
+    }
+
+    public void setDistrictCode(String districtCode) {
+        this.districtCode = districtCode;
+    }
+
+
+    public String getTehsilCode() {
+        return tehsilCode;
+    }
+
+    public void setTehsilCode(String tehsilCode) {
+        this.tehsilCode = tehsilCode;
+    }
+
+
+    public String getUcCode() {
+        return ucCode;
+    }
+
+    public void setUcCode(String ucCode) {
+        this.ucCode = ucCode;
+    }
+
+
+    public String getHfCode() {
+        return hfCode;
+    }
+
+    public void setHfCode(String hfCode) {
+        this.hfCode = hfCode;
     }
 
 
@@ -870,6 +906,11 @@ public class ModuleI extends BaseObservable implements Observable {
     public ModuleI Hydrate(Cursor cursor) throws JSONException {
         this.id = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_ID));
         this.uid = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_UID));
+        this.uuid = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_UUID));
+        this.districtCode = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_DISTRICT_CODE));
+        this.tehsilCode = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_TEHSIL_CODE));
+        this.ucCode = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_UC_CODE));
+        this.hfCode = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_HF_CODE));
         this.userName = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_USERNAME));
         this.sysDate = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_SYSDATE));
         this.deviceId = cursor.getString(cursor.getColumnIndexOrThrow(ModuleITable.COLUMN_DEVICEID));
@@ -949,6 +990,11 @@ public class ModuleI extends BaseObservable implements Observable {
         JSONObject json = new JSONObject();
         json.put(ModuleITable.COLUMN_ID, this.id);
         json.put(ModuleITable.COLUMN_UID, this.uid);
+        json.put(ModuleITable.COLUMN_UUID, this.uuid);
+        json.put(ModuleITable.COLUMN_DISTRICT_CODE, this.districtCode);
+        json.put(ModuleITable.COLUMN_TEHSIL_CODE, this.tehsilCode);
+        json.put(ModuleITable.COLUMN_UC_CODE, this.ucCode);
+        json.put(ModuleITable.COLUMN_HF_CODE, this.hfCode);
         json.put(ModuleITable.COLUMN_USERNAME, this.userName);
         json.put(ModuleITable.COLUMN_SYSDATE, this.sysDate);
         json.put(ModuleITable.COLUMN_DEVICEID, this.deviceId);
